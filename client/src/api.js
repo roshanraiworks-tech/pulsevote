d6fb8057fc416bc7c7cefa09726f75b4c7579503
ROSHANxCODER <noneedofcreating@gmail.com>
2026-05-12 09:30:10 +0530

    Last stage

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_SERVER_URL || "http://localhost:5000",
+    baseURL: import.meta.env.VITE_SERVER_URL || "https://pulsevote-rr0y.onrender.com",
     headers: {
         "Content-Type": "application/json",
     },
