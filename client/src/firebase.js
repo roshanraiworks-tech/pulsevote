bdc8564a966d9d227c8d0fdbed46ff64d5b0b4b6
ROSHANxCODER <noneedofcreating@gmail.com>
2026-05-15 21:47:15 +0530

    last time

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,39 +6,41 @@ import {
 } from "firebase/auth";
 // import { getAuth } from 'firebase/auth'
 
-const requiredKeys = [
-    "VITE_FIREBASE_API_KEY",
-    "VITE_FIREBASE_AUTH_DOMAIN",
-    "VITE_FIREBASE_PROJECT_ID",
-    "VITE_FIREBASE_STORAGE_BUCKET",
-    "VITE_FIREBASE_MESSAGING_SENDER_ID",
-    "VITE_FIREBASE_APP_ID",
-];
-
-for (const key of requiredKeys) {
-    if (!import.meta.env[key]) {
-        throw new Error(`Missing environment variable: ${key}`);
-    }
-}
-
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+const rconst firebaseConfig = {
+    apiKey: "AIzaSyBXL6Wo5gtrkxYu-NRcZyYy29n7fkU8qFM",
+    authDomain: "pulsevote-117.firebaseapp.com",
+    projectId: "pulsevote-117",
+    storageBucket: "pulsevote-117.firebasestorage.app",
+    messagingSenderId: "264797894885",
+    appId: "1:264797894885:web:b66d365fb980706307bc0c",
+    measurementId: "G-M09KD98KCY"
 };
 
-const app = initializeApp(firebaseConfig)
-console.log("FIREBASE KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
+// console.log("FIREBASE KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
 
+// for (const key of requiredKeys) {
+//     if (!import.meta.env[key]) {
+//         throw new Error(`Missing environment variable: ${key}`);
+//     }
+// }
+
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+
+//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+
+//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+
+//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+
+//   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+
+//   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+// };
+
+const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
+console.log("FIREBASE KEY:", import.meta.env.VITE_FIREBASE_API_KEY);
 setPersistence(auth, browserSessionPersistence)
     .then(() => {
         console.log("Session persistence enabled");
